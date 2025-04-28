8c2a4bab99ead0a5a43ece950deb7a0414517e8f
avishkaindunil <haavishkaindunil@gmail.com>
2025-04-28 13:03:23 +0530

    adding variables with primitive data types

--- Hello.java
+++ Hello.java
@@ -1,10 +1,27 @@
 class Hello {
-    public static void main(String a[]) {
+    public static void main(String[] args) {
         System.out.println("Hello, World!\n");
 
-        int num1 = 5;
+        int num1 = 5; // assign a variable
         int num2 = 10;
         int result = num1 + num2;
+
+        int num3 = 15;
+        byte by = 127;
+        long l = 123456789L; 
+        short s = 12345;
+
+        float f = 123.45f;
+        double d = 123.456789;
+
+        char c = 'A';
+
+        boolean b = true;
+        String str = "Hello, Java!";
+
+        System.out.println("The value of num3, by, l, s, f, d, c, b and str are: " 
+                           + num3 + ", " + by + ", " + l + ", " + s + ", " 
+                           + f + ", " + d + ", " + c + ", " + b + ", " + str);
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
     }
