b75df6b6b7d34d189f9b8a798f66c316e5602511
avishkaindunil <haavishkaindunil@gmail.com>
2025-04-28 13:31:18 +0530

    Type conversion and casting

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!\n");
+    public static void main(String a[]) {
+        System.out.println("Hello, World!");
 
         int num1 = 5; // assign a variable
         int num2 = 10;
@@ -8,10 +8,10 @@ class Hello {
 
         int num3 = 15;
         byte by = 127;
-        long l = 123456789L; 
+        long l = 123456789L;
         short s = 12345;
 
-        float f = 123.45f;
+        float f = 123.45F;
         double d = 123.456789;
 
         char c = 'A';
@@ -19,9 +19,7 @@ class Hello {
         boolean b = true;
         String str = "Hello, Java!";
 
-        System.out.println("The value of num3, by, l, s, f, d, c, b and str are: " 
-                           + num3 + ", " + by + ", " + l + ", " + s + ", " 
-                           + f + ", " + d + ", " + c + ", " + b + ", " + str);
+        System.out.println("The value of num3, by, l, s, f, d, c, b and str are: " + num3 + ", " + by + ", " + l + ", " + s + ", " + f + ", " + d + ", " + c + ", " + b + ", " + str);
 
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
     }
