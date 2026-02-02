5bcf478e70c99c0f7603447564ddcc88047f3f3f
avishkaindunil <haavishkaindunil@gmail.com>
2026-02-02 21:01:47 +0530

    Sample Exercise Two

---
+++ grossPayCalculator.java
@@ -0,0 +1,9 @@
+public class grossPayCalculator {
+    public static void main(String[] args){
+        int hours = 40;
+        double payRate = 22.50;
+        double grossPay = hours * payRate;
+
+        System.out.println("Gross Pay: " + grossPay);
+    }
+}
