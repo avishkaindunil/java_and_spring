6a5cee944403d044cb5fc9af900570227d4f7a6c
avishkaindunil <haavishkaindunil@gmail.com>
2026-02-05 20:29:25 +0530

    Changed the codebase - Add SWITCH EXPRESSION

--- grossPayCalculator.java
+++ grossPayCalculator.java
@@ -17,14 +17,30 @@ public class grossPayCalculator {
         System.out.print("How many sales did you make this week: ");
         int sales = scanner.nextInt();
 
+        System.out.print("Enter your letter grade: ");
+        String grade = scanner.next();
+
         scanner.close();
 
         double grossPay = hours * payRate;
 
+        String message = switch(grade){
+            case "A" -> "Exellent Job!";
+            case "B" -> "Great Job!";
+            case "C" -> "Good Job!";
+            case "D" -> "you need to work bit harder";
+            case "F" -> "uh oh!";
+            default -> "Error! Invalid Grade";
+        };
+
         if(sales >= quota){
             System.out.println("Congrats! You've met your quota.");
+        } else {
+            int salesShort = quota - sales;
+            System.out.println("You did not make your quota. You were " + salesShort + " sales shorts.");
         }
 
+        System.out.println(message);
         System.out.println("Gross Pay: " + grossPay +"$");
     }
 }
