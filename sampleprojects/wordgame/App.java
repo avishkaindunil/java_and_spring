f5c6b4937a051ae8db68d3f91f74ed72d5b06b15
Avishka Indunil Kalupahana <118374198+avishkaindunil@users.noreply.github.com>
2026-08-22 23:44:26 +0530

    Building a word guessing game - part 1

---
+++ sampleprojects/wordgame/App.java
@@ -0,0 +1,11 @@
+package sampleprojects.wordgame;
+
+public class App {
+    public static void main(String[] args){
+        String words[] = {"java", "programming", "class", "computer", "method"};
+
+        WordGuessingGame game = new WordGuessingGame(words, 10);
+
+        game.play();
+    }
+}
